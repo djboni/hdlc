cb2997fb39f03914b83532699deb84fb0f333dd6
Djones A. Boni <djboni@gmail.com>
2016-09-23 14:37:35 -0300

    One Byte Trasport Layer

---
+++ 4_TL/HDLC_1BTL.cpp
@@ -0,0 +1,106 @@
+/*
+ Copyright 2016 Djones A. Boni
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+#include "HDLC_1BTL.h"
+#include <string.h>
+
+#include "Arduinutil.h"
+
+#define MASK    0xC0U
+#define MASKINV 0x3FU
+#define RESET   0x00U
+#define ACK     0x40U
+#define NACK    0x80U
+#define DATA    0xC0U
+
+static const uint8_t SEQ_MAX = 62U;
+
+HDLC_1BTL::HDLC_1BTL()
+{
+    init();
+}
+
+void HDLC_1BTL::init()
+{
+    HDLC::init();
+    count_seq = SEQ_MAX;
+    count_tx_noack = 0U;
+}
+
+void HDLC_1BTL::transmitReset()
+{
+    init();
+    HDLC::transmitStart();
+    HDLC::transmitByte(RESET);
+    HDLC::transmitEnd();
+}
+
+void HDLC_1BTL::transmitAck(uint8_t rxs)
+{
+    rxs &= MASKINV;
+    rxs |= ACK;
+    HDLC::transmitStart();
+    HDLC::transmitByte(rxs);
+    HDLC::transmitEnd();
+}
+
+void HDLC_1BTL::transmitNack(uint8_t rxs)
+{
+    rxs &= MASKINV;
+    rxs |= NACK;
+    HDLC::transmitStart();
+    HDLC::transmitByte(rxs);
+    HDLC::transmitEnd();
+}
+
+uint16_t HDLC_1BTL::receive()
+{
+    uint16_t datalen = HDLC::receive();
+    if(datalen != 0U)
+    {
+        datalen -= 1U;
+
+        uint8_t frameseq;
+        HDLC::copyReceivedMessage(&frameseq, 0U, 1U);
+
+        uint8_t frame = frameseq & MASK;
+        uint8_t rxs = frameseq & MASKINV;
+        if(frame == DATA)
+        {
+            transmitAck(rxs);
+        }
+        else if(frame == ACK)
+        {
+            count_tx_noack = 0U;
+        }
+        else if(frame == NACK)
+        {
+        }
+        else if(frame == RESET)
+        {
+        }
+        else
+        {
+        }
+    }
+    return datalen;
+}
+
+uint16_t HDLC_1BTL::copyReceivedMessage(uint8_t (&buff)[RXBFLEN])
+{
+    uint16_t datalen = HDLC::copyReceivedMessage(buff, 1U, RXBFLEN - 1U, true);
+    return datalen;
+}
