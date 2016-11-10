27f0d549a56bd75c4032bcfc1a0d68c984ca5c47
Djones A. Boni <djboni@gmail.com>
2016-11-09 22:03:09 -0200

    HDLC RX

--- HDLC.py
+++ HDLC.py
@@ -2,6 +2,13 @@
 # -*- coding: utf-8 -*-
 
 class HDLC:
+
+    class STAT:
+        ESCAPED = -1
+        RECEIVING = 0
+        OK = 1
+        CRCERR = 2
+
     def __init__(self, readByte=None, writeByte=None, CRC=None):
         if False in (readByte != None, writeByte != None, CRC != None):
             raise ValueError('Must use the three parameters function readByte, function readByte, anc CRC class')
@@ -9,14 +16,13 @@ class HDLC:
         self.writeByte = writeByte
         self.crc = CRC()
         self.txcrc = CRC()
+        self.msg_queue = []
         self.init()
 
     def init(self):
-        self.data = []
+        self.data = ''
         self.status = 0
         self.crc.init()
-
-        self.msg_queue = []
 
         self.DATAINVBIT = 0x20
         self.DATASTART = '~'
@@ -53,6 +59,40 @@ class HDLC:
             self.escapeAndWriteByte(chr(self.txcrc[i]))
         self.writeByte(self.DATASTART)
 
+    def receive(self):
+        c = self.readByte()
+        if c == '':
+            return 0
+
+        if self.status >= self.STAT.OK:
+            self.init()
+
+        retv = 0
+
+        if c == self.DATASTART:
+            if self.status == self.STAT.RECEIVING and len(self.data) != 0:
+                if self.crc.good():
+                    self.status = self.STAT.OK
+                    self.data = self.data[:-self.crc.size]
+                    retv = len(self.data)
+                    self.msg_queue.append(self.data)
+                else:
+                    self.status = self.STAT.CRCERR
+            else:
+                self.init()
+        else:
+            if self.status == self.STAT.ESCAPED:
+                self.status = self.STAT.RECEIVING
+                c = chr(ord(c) ^ self.DATAINVBIT)
+                self.crc.update(c)
+                self.data += c
+            elif c != self.DATAESCAPE:
+                self.crc.update(c)
+                self.data += c
+            else:
+                self.status = self.STAT.ESCAPED
+
+        return retv
 
 if __name__ == '__main__':
     def f_read(x=['~\x00\x78\xF0~' + '~}^}]\xF1\xCD~']):
@@ -89,4 +129,14 @@ if __name__ == '__main__':
     print(s)
     print(x == '~\x00\x78\xF0~' + '~}^}]\xF1\xCD~')
 
-    # TODO RX
+    # RX
+    while hdlc.receive() == 0:
+        pass
+    print(hdlc.msg_queue)
+    print(hdlc.msg_queue == ['\x00'])
+
+    # RX 2
+    while hdlc.receive() == 0:
+        pass
+    print(hdlc.msg_queue)
+    print(hdlc.msg_queue == ['\x00', '~}'])
